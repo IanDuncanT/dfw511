c5ebfdd35a8f6fc6b859670f308fd5165aef24c8
Ian Duncan <duncan.ian.t@gmail.com>
2019-07-23 20:08:48 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'dfw511',
   packages = ['dfw511'],
-  version = '0.0.2',
+  version = '0.0.4',
   license='MIT',
   description = 'Python library to retrieve information and convert to json from 511dfw.org',
   author = 'Ian Duncan',
   author_email = 'duncan.ian.t@gmail.com',
   url = 'https://github.com/IanDuncanT/dfw511',
-  download_url = 'https://github.com/IanDuncanT/dfw511/archive/v0.0.2.tar.gz',
+  download_url = 'https://github.com/IanDuncanT/dfw511/archive/v0.0.4.tar.gz',
   keywords = ['Traffic', 'DFW', '511', 'Traffic Information', '511dfw', 'DFW 511'],
   install_requires=[
           'requests',
